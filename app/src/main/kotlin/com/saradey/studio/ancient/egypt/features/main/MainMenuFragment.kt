f7cf718a8336b53f9726c3f6dbe1e85cc9bf018d
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-13 01:33:54 +0300

    мелкие правки

--- app/src/main/kotlin/com/saradey/studio/ancient/egypt/features/main/MainMenuFragment.kt
+++ app/src/main/kotlin/com/saradey/studio/ancient/egypt/features/main/MainMenuFragment.kt
@@ -3,11 +3,15 @@ package com.saradey.studio.ancient.egypt.features.main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.saradey.studio.ancient.egypt.R
+import com.saradey.studio.ancient.egypt.databinding.FragmentMainMenuBinding
 
 class MainMenuFragment : Fragment(R.layout.fragment_main_menu) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    private val binding: FragmentMainMenuBinding by viewBinding(FragmentMainMenuBinding::bind)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        binding.btnMenu.setOnApplyWindowInsetsListener(null)
     }
 }
