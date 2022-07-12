ff1f83f099cae10dea7a833392a40604bce4a9aa
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-13 00:18:22 +0300

    Добавили новую иконку, созданы фрагменты заглушки

---
+++ app/src/main/kotlin/com/saradey/studio/ancient/egypt/features/authorization/AuthorizationFragment.kt
@@ -0,0 +1,13 @@
+package com.saradey.studio.ancient.egypt.features.authorization
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.saradey.studio.ancient.egypt.R
+
+class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
---
+++ app/src/main/kotlin/com/saradey/studio/ancient/egypt/features/settings/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package com.saradey.studio.ancient.egypt.features.settings
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.saradey.studio.ancient.egypt.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
