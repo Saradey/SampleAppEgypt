e6c3b3af01e5afe687bad7ded086152b5281f5e8
Evgeny Goncharov <jtgn@yandex.ru>
2022-07-13 17:12:09 +0300

    заполнение sandbox дизайн системой

--- ui-sandbox/src/main/kotlin/com/saradey/studio/ancient/egypt/ui_sandbox/MainUiSandboxActivity.kt
+++ ui-sandbox/src/main/kotlin/com/saradey/studio/ancient/egypt/ui_sandbox/MainUiSandboxActivity.kt
@@ -1,6 +1,5 @@
 package com.saradey.studio.ancient.egypt.ui_sandbox
 
-import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -15,8 +14,5 @@ class MainUiSandboxActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_ui_sandbox)
-        binding.imv.setOnClickListener {
-            (binding.imv.drawable as AnimatedVectorDrawable).start()
-        }
     }
 }
